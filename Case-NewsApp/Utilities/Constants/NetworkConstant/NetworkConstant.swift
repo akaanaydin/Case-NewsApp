45b92d978635b31e2d8ba260926e1ccefc5e74a3
A. Kaan AYDIN <44496296+akaanaydin@users.noreply.github.com>
2023-01-05 00:01:33 +0300

    Update NetworkConstant.swift

--- Case-NewsApp/Utilities/Constants/NetworkConstant/NetworkConstant.swift
+++ Case-NewsApp/Utilities/Constants/NetworkConstant/NetworkConstant.swift
@@ -14,7 +14,7 @@ extension Constant {
         enum ServiceEndPoint: String {
             
             case BASE_URL = "https://newsapi.org/v2/everything?q=apple&from=2022-06-17&to=2022-06-17&sortBy=popularity&"
-            case API_KEY = "apiKey=476d05192e6c42e1b9ffacf403f3dbbe"
+            case API_KEY = "apiKey=YOUR_API_KEY"
             
             static func fetchArticles() -> String {
                 "\(ServiceEndPoint.BASE_URL.rawValue)\(ServiceEndPoint.API_KEY.rawValue)"
